d37f635267cb81453d6eb5204b141b8513b5ee40
Eric Bresler <ebresler@live.fr>
2021-07-17 22:28:16 +0200

    Adding empty tests

--- mainsite/mainsite/settings/development.py
+++ mainsite/mainsite/settings/development.py
@@ -10,18 +10,19 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'default_db',
-        'USER': 'user',
-        'PASSWORD': 'password',
-        'HOST': '',
+        'NAME': 'booking_database',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
         'PORT': '5432',
     },
     'purbeurre': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'other_db',
-        'USER': 'user',
-        'PASSWORD': 'password',
-        'HOST': '',
+        'NAME': 'purbeurre_database',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
